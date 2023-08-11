88e965aa15083f61f3521bbdceef7dbfdce9fd70
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2023-08-10 21:52:35 -0300

    Feito adaptador de aluno

---
+++ lib/adaptadores/salvar_aluno_local.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+import 'package:diario_de_classe/dto/alunoDTO.dart';
+import 'package:diario_de_classe/dto/turmaDTO.dart';
+import 'package:diario_de_classe/dto/turma_alunoDTO.dart';
+import 'package:diario_de_classe/portas/saida/i_aluno.dart';
+import 'package:diario_de_classe/portas/saida/i_turma.dart';
+
+class SalvarAlunoLocal implements IAluno {
+  @override
+  AlunoDTO salvar(AlunoDTO alunoDTO) {
+    // TODO: implement salvar
+    throw UnimplementedError();
+  }
+
+}
